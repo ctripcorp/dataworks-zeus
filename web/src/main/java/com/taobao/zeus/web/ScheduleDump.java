f5b800ac28d048ca6489dffe141c57e3db7690f6
kylong <kylong@ctrip.com>
2014-10-30 16:29:12 +0800

    clear history action

--- web/src/main/java/com/taobao/zeus/web/ScheduleDump.java
+++ web/src/main/java/com/taobao/zeus/web/ScheduleDump.java
@@ -55,134 +55,201 @@ import com.taobao.zeus.store.mysql.persistence.JobPersistenceBackup;
 import com.taobao.zeus.store.mysql.persistence.JobPersistenceOld;
 import com.taobao.zeus.store.mysql.tool.PersistenceAndBeanConvert;
 import com.taobao.zeus.util.Tuple;
+
 /**
  * Dump调度系统内的Job状态，用来调试排查问题
+ * 
  * @author zhoufang
- *
+ * 
  */
-public class ScheduleDump extends HttpServlet  {
+public class ScheduleDump extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	private DistributeLocker locker;
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
-		ApplicationContext context=WebApplicationContextUtils.getWebApplicationContext(config.getServletContext());
-		locker=(DistributeLocker) context.getBean("distributeLocker");
+		ApplicationContext context = WebApplicationContextUtils
+				.getWebApplicationContext(config.getServletContext());
+		locker = (DistributeLocker) context.getBean("distributeLocker");
 	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		resp.setContentType("text/html");
 		resp.setCharacterEncoding("utf-8");
 		try {
-			if(locker!=null){
-				Field zeusScheduleField=locker.getClass().getDeclaredField("zeusSchedule");
+			if (locker != null) {
+				Field zeusScheduleField = locker.getClass().getDeclaredField(
+						"zeusSchedule");
 				zeusScheduleField.setAccessible(true);
-				ZeusSchedule zeusSchedule=(ZeusSchedule) zeusScheduleField.get(locker);
-				if(zeusSchedule!=null){
-					Field masterContextField=zeusSchedule.getClass().getDeclaredField("context");
+				ZeusSchedule zeusSchedule = (ZeusSchedule) zeusScheduleField
+						.get(locker);
+				if (zeusSchedule != null) {
+					Field masterContextField = zeusSchedule.getClass()
+							.getDeclaredField("context");
 					masterContextField.setAccessible(true);
-					MasterContext context=(MasterContext)masterContextField.get(zeusSchedule);
-					if(context!=null){
-						String op=req.getParameter("op");
-						if("workers".equals(op)){
-							Map<Channel, MasterWorkerHolder> workers=context.getWorkers();
-							SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-							for(Channel channel:workers.keySet()){
-								MasterWorkerHolder holder=workers.get(channel);
-								Set<String> runnings=holder.getRunnings().keySet();
-								Set<String> manualRunnings=holder.getManualRunnings().keySet();
-								Set<String> debugRunnings=holder.getDebugRunnings().keySet();
-								HeartBeatInfo heart=holder.getHeart();
-								resp.getWriter().println("<br>"+channel.getRemoteAddress()+":");
-								resp.getWriter().println("<br>"+"\t runnings:"+runnings.toString());
-								resp.getWriter().println("<br>"+"\t manual runnings:"+manualRunnings.toString());
-								resp.getWriter().println("<br>"+"\t debug runnings:"+debugRunnings.toString());
-								resp.getWriter().println("<br>"+"\t heart beat: ");
-								resp.getWriter().println("<br>"+"\t\t last heartbeat:"+ format.format(heart.timestamp));
-								resp.getWriter().println("<br>"+"\t\t mem use rate:"+heart.memRate);
-								resp.getWriter().println("<br>"+"\t\t runnings:"+heart.runnings.toString());
-								resp.getWriter().println("<br>"+"\t\t manual runnings:"+heart.manualRunnings.toString());
-								resp.getWriter().println("<br>"+"\t\t debug runnings:"+heart.debugRunnings.toString());
+					MasterContext context = (MasterContext) masterContextField
+							.get(zeusSchedule);
+					if (context != null) {
+						String op = req.getParameter("op");
+						if ("workers".equals(op)) {
+							Map<Channel, MasterWorkerHolder> workers = context
+									.getWorkers();
+							SimpleDateFormat format = new SimpleDateFormat(
+									"yyyy-MM-dd HH:mm:ss");
+							for (Channel channel : workers.keySet()) {
+								MasterWorkerHolder holder = workers
+										.get(channel);
+								Set<String> runnings = holder.getRunnings()
+										.keySet();
+								Set<String> manualRunnings = holder
+										.getManualRunnings().keySet();
+								Set<String> debugRunnings = holder
+										.getDebugRunnings().keySet();
+								HeartBeatInfo heart = holder.getHeart();
+								resp.getWriter().println(
+										"<br>" + channel.getRemoteAddress()
+												+ ":");
+								resp.getWriter().println(
+										"<br>" + "\t runnings:"
+												+ runnings.toString());
+								resp.getWriter().println(
+										"<br>" + "\t manual runnings:"
+												+ manualRunnings.toString());
+								resp.getWriter().println(
+										"<br>" + "\t debug runnings:"
+												+ debugRunnings.toString());
+								resp.getWriter().println(
+										"<br>" + "\t heart beat: ");
+								resp.getWriter()
+										.println(
+												"<br>"
+														+ "\t\t last heartbeat:"
+														+ format.format(heart.timestamp));
+								resp.getWriter().println(
+										"<br>" + "\t\t mem use rate:"
+												+ heart.memRate);
+								resp.getWriter().println(
+										"<br>" + "\t\t runnings:"
+												+ heart.runnings.toString());
+								resp.getWriter().println(
+										"<br>"
+												+ "\t\t manual runnings:"
+												+ heart.manualRunnings
+														.toString());
+								resp.getWriter().println(
+										"<br>"
+												+ "\t\t debug runnings:"
+												+ heart.debugRunnings
+														.toString());
 							}
-						}else if("queue".equals(op)){
-							Queue<JobElement> queue=context.getQueue();
-							Queue<JobElement> debugQueue=context.getDebugQueue();
-							Queue<JobElement> manualQueue=context.getManualQueue();
-							resp.getWriter().println("<br>"+"schedule jobs in queue:");
-							for(JobElement jobId:queue){
-								resp.getWriter().print(jobId.getJobID()+"\t");
+						} else if ("queue".equals(op)) {
+							Queue<JobElement> queue = context.getQueue();
+							Queue<JobElement> debugQueue = context
+									.getDebugQueue();
+							Queue<JobElement> manualQueue = context
+									.getManualQueue();
+							resp.getWriter().println(
+									"<br>" + "schedule jobs in queue:");
+							for (JobElement jobId : queue) {
+								resp.getWriter().print(jobId.getJobID() + "\t");
 							}
-							resp.getWriter().println("<br>"+"manual jobs in queue:");
-							for(JobElement jobId:manualQueue){
-								resp.getWriter().print(jobId.getJobID()+"\t");
+							resp.getWriter().println(
+									"<br>" + "manual jobs in queue:");
+							for (JobElement jobId : manualQueue) {
+								resp.getWriter().print(jobId.getJobID() + "\t");
 							}
-							resp.getWriter().println("<br>"+"debug jobs in queue:");
-							for(JobElement jobId:debugQueue){
-								resp.getWriter().print(jobId.getJobID()+"\t");
+							resp.getWriter().println(
+									"<br>" + "debug jobs in queue:");
+							for (JobElement jobId : debugQueue) {
+								resp.getWriter().print(jobId.getJobID() + "\t");
 							}
-						}else if("jobstatus".equals(op)){
-							Dispatcher dispatcher=context.getDispatcher();
-							if(dispatcher!=null){
-								for(Controller c:dispatcher.getControllers()){
-									resp.getWriter().println("<br>"+c.toString());
+						} else if ("jobstatus".equals(op)) {
+							Dispatcher dispatcher = context.getDispatcher();
+							if (dispatcher != null) {
+								for (Controller c : dispatcher.getControllers()) {
+									resp.getWriter().println(
+											"<br>" + c.toString());
 								}
 							}
-						}else if("clearschedule".equals(op)){
+						} else if ("clearschedule".equals(op)) {
 							Date now = new Date();
-							SimpleDateFormat df=new SimpleDateFormat("yyyyMMddHHmmss");
-							String currentDateStr = df.format(now)+"0000";
-							Dispatcher dispatcher=context.getDispatcher();
-							if(dispatcher!=null){
-								List<Controller> controllers = dispatcher.getControllers();
-								if(controllers != null && controllers.size()>0){
-									for(Controller c : controllers){
-										JobController jobc = (JobController)c;
+							SimpleDateFormat df = new SimpleDateFormat(
+									"yyyyMMddHHmmss");
+							String currentDateStr = df.format(now) + "0000";
+							Dispatcher dispatcher = context.getDispatcher();
+							if (dispatcher != null) {
+								List<Controller> controllers = dispatcher
+										.getControllers();
+								if (controllers != null
+										&& controllers.size() > 0) {
+									for (Controller c : controllers) {
+										JobController jobc = (JobController) c;
 										String jobId = jobc.getJobId();
-										if(Long.parseLong(jobId)<Long.parseLong(currentDateStr)){
-											context.getScheduler().deleteJob(jobId, "zeus");
+										if (Long.parseLong(jobId) < Long
+												.parseLong(currentDateStr)) {
+											context.getScheduler().deleteJob(
+													jobId, "zeus");
 										}
 									}
 								}
 							}
 							resp.getWriter().println("清理完毕！");
-							
-						}else if("action".equals(op)){
+
+						} else if ("action".equals(op)) {
 							Date now = new Date();
-							SimpleDateFormat df2=new SimpleDateFormat("yyyy-MM-dd");
-							SimpleDateFormat df3=new SimpleDateFormat("yyyyMMddHHmmss");
-							String currentDateStr = df3.format(now)+"0000";
-							List<JobPersistenceOld> jobDetails = context.getGroupManagerOld().getAllJobs();
+							SimpleDateFormat df2 = new SimpleDateFormat(
+									"yyyy-MM-dd");
+							SimpleDateFormat df3 = new SimpleDateFormat(
+									"yyyyMMddHHmmss");
+							String currentDateStr = df3.format(now) + "0000";
+							List<JobPersistenceOld> jobDetails = context
+									.getGroupManagerOld().getAllJobs();
 							Map<Long, JobPersistence> actionDetails = new HashMap<Long, JobPersistence>();
-							context.getMaster().runScheduleJobToAction(jobDetails, now, df2, actionDetails, currentDateStr);
-							context.getMaster().runDependencesJobToAction(jobDetails, actionDetails, currentDateStr, 0);
-							
-							Dispatcher dispatcher=context.getDispatcher();
-							if(dispatcher != null){
-								//增加controller，并修改event
+							context.getMaster().runScheduleJobToAction(
+									jobDetails, now, df2, actionDetails,
+									currentDateStr);
+							context.getMaster().runDependencesJobToAction(
+									jobDetails, actionDetails, currentDateStr,
+									0);
+
+							Dispatcher dispatcher = context.getDispatcher();
+							if (dispatcher != null) {
+								// 增加controller，并修改event
 								if (actionDetails.size() > 0) {
 									for (Long id : actionDetails.keySet()) {
-										dispatcher.addController(
-												new JobController(context, context.getMaster(),
+										dispatcher
+												.addController(new JobController(
+														context, context
+																.getMaster(),
 														id.toString()));
 										if (id > Long.parseLong(currentDateStr)) {
-											context.getDispatcher().forwardEvent(
-													new JobMaintenanceEvent(Events.UpdateJob,
-															id.toString()));
+											context.getDispatcher()
+													.forwardEvent(
+															new JobMaintenanceEvent(
+																	Events.UpdateJob,
+																	id.toString()));
 										}
 									}
 								}
-							
-								//清理schedule
-								List<Controller> controllers = dispatcher.getControllers();
-								if(controllers!=null && controllers.size()>0){
-									for(Controller c : controllers){
-										JobController jobc = (JobController)c;
+
+								// 清理schedule
+								List<Controller> controllers = dispatcher
+										.getControllers();
+								if (controllers != null
+										&& controllers.size() > 0) {
+									for (Controller c : controllers) {
+										JobController jobc = (JobController) c;
 										String jobId = jobc.getJobId();
-										if(Long.parseLong(jobId)<Long.parseLong(currentDateStr)){
+										if (Long.parseLong(jobId) < Long
+												.parseLong(currentDateStr)) {
 											try {
-												context.getScheduler().deleteJob(jobId, "zeus");
+												context.getScheduler()
+														.deleteJob(jobId,
+																"zeus");
 											} catch (SchedulerException e) {
 												e.printStackTrace();
 											}
@@ -191,103 +258,101 @@ public class ScheduleDump extends HttpServlet  {
 								}
 							}
 							resp.getWriter().println("Action生成完毕！");
-						}else if("clear".equals(op)){
-							 int beforeAmount = -1;
-							 int cnt = 0;
-							 Calendar cal = Calendar.getInstance();
-							 cal.add(Calendar.HOUR, beforeAmount);
-							 Date date = cal.getTime();
-							 SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
-							 String dateStr = df.format(date)+"0000";
-							 Dispatcher dispatcher = context.getDispatcher();
-							 if (dispatcher != null) {
-								 List<Controller> controllers = dispatcher.getControllers();
-									if(controllers != null && controllers.size()>0){
-										List<JobDescriptor> toBeTransferred = new ArrayList<JobDescriptor>();
-										Iterator<Controller> iter = controllers.iterator();
-										while(iter.hasNext()){
-											JobController jobc = (JobController)iter.next();
-											String jobId = jobc.getJobId();
-											if (Long.parseLong(jobId) < Long.parseLong(dateStr)) {
-												Tuple<JobDescriptor, JobStatus> tuple = context.getGroupManager().getJobDescriptor(jobId);
-												JobStatus status = tuple.getY();
-												if (!Status.RUNNING.equals(status.getStatus())) {
-													toBeTransferred.add(tuple.getX());
-													iter.remove();
-													cnt++;	
-												}
-											}										
+						} else if ("clear".equals(op)) {
+							int beforeAmount = -2;
+							int cnt = 0;
+							int sum = 0;
+							Calendar cal = Calendar.getInstance();
+							cal.add(Calendar.MONTH, beforeAmount);
+							Date date = cal.getTime();
+							SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+							String dateStr = df.format(date) + "0000";
+							Dispatcher dispatcher = context.getDispatcher();
+							if (dispatcher != null) {
+								List<Controller> controllers = dispatcher.getControllers();
+								if (controllers != null&& controllers.size() > 0) {
+									resp.getWriter().println("开始清理内存中controller id为" + dateStr + "以前的controller：");
+									List<JobDescriptor> toBeTransferred = new ArrayList<JobDescriptor>();
+									Iterator<Controller> iter = controllers.iterator();
+									while (iter.hasNext()) {
+										sum ++ ;
+										JobController jobc = (JobController) iter.next();
+										String jobId = jobc.getJobId();
+										if (Long.parseLong(jobId) < Long.parseLong(dateStr)) {
+											Tuple<JobDescriptor, JobStatus> tuple = context.getGroupManager().getJobDescriptor(jobId);
+											JobStatus status = tuple.getY();
+											if (!Status.RUNNING.equals(status.getStatus())) {
+												toBeTransferred.add(tuple.getX());
+												iter.remove();
+												resp.getWriter().println("<br>成功清理了id为" + jobId + "的controller");
+												cnt++;
+											}
 										}
-										resp.getWriter().println("清理了内存中一个月前"+cnt+"个controllers");
-									if (toBeTransferred != null
-											&& toBeTransferred.size() != 0) {
-										MysqlGroupManager manager = (MysqlGroupManager) context
-												.getApplicationContext()
-												.getBean("groupManager");
-										HibernateTemplate template = manager
-												.getHibernateTemplate();
-										SessionFactory factory = template
-												.getSessionFactory();
+									}
+									resp.getWriter().println("<br>内存中共"+ sum +"个controllers，清理了两个月前" + cnt+ "个controllers");
+									if (toBeTransferred != null && toBeTransferred.size() != 0) {
+										int count = 0;
+										MysqlGroupManager manager = ( MysqlGroupManager ) context.getApplicationContext ().getBean( "groupManager" );
+										HibernateTemplate template = manager.getHibernateTemplate();
+										SessionFactory factory = template.getSessionFactory();
 										Session session = factory.openSession();
 										Transaction tx = null;
 										tx = session.beginTransaction();
 										try {
 											for (JobDescriptor job : toBeTransferred) {
-												JobPersistence persist = PersistenceAndBeanConvert
-														.convert(job);
-												JobPersistenceBackup backup = new JobPersistenceBackup(
-														persist);
+												JobPersistence persist = PersistenceAndBeanConvert.convert(job);
+												JobPersistenceBackup backup = new JobPersistenceBackup(persist);
+												resp.getWriter().println("<br>开始备份数据库中id为" + job.getId() + "的action");
 												session.delete(persist);
-												session.save(backup);
+												session.saveOrUpdate(backup);
+												count ++;
 											}
 											tx.commit();
+											resp.getWriter().println("<br>完成数据库备份， 共备份" + count + "条数据");
 										} catch (RuntimeException e) {
 											try {
 												tx.rollback();
 											} catch (Exception e2) {
-												resp.getWriter().println(
-														e2.getMessage());
+												resp.getWriter().println(e2.getMessage());
 											}
-											resp.getWriter().println(
-													e.getMessage());
+											resp.getWriter().println(e.getMessage());
 										} finally {
 											try {
-												if (session != null){
+												if (session != null) {
 													session.close();
 												}
-												if (factory != null){
+												if (factory != null) {
 													factory.close();
 												}
 											} catch (Exception e3) {
-												resp.getWriter().print(
-														e3.getMessage());
+												resp.getWriter().print(e3.getMessage());
 											}
 										}
-									} 
-										resp.getWriter().println("已将备份一个月前的action列表");
 									}
+
+								}
 							}
-							
+
 						}
-						
-						else{
+
+						else {
 							resp.getWriter().println("<a href='dump.do?op=jobstatus'>查看Job调度状态</a>&nbsp;&nbsp;&nbsp;&nbsp;");
 							resp.getWriter().println("<a href='dump.do?op=clearschedule' >清理Job调度信息</a>&nbsp;&nbsp;&nbsp;&nbsp;");
 							resp.getWriter().println("<a href='dump.do?op=workers'>查看master-worker 状态</a>&nbsp;&nbsp;&nbsp;&nbsp;");
 							resp.getWriter().println("<a href='dump.do?op=queue' >等待队列任务</a>&nbsp;&nbsp;&nbsp;&nbsp;");
-							resp.getWriter().println("<a href='dump.do?op=action' >生成Action版本</a>");
-							resp.getWriter().println("<a href='dump.do?op=clearcontroller' >清理和备份一个月前action</a>");
-							
+							resp.getWriter().println("<a href='dump.do?op=action' >生成Action版本</a>&nbsp;&nbsp;&nbsp;&nbsp;");
+							resp.getWriter().println("<a href='dump.do?op=clear' >清理和备份两个月前job</a>");
+
 						}
 					}
-					
+
 				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		resp.getWriter().close();
-//		req.getRequestDispatcher("/login.jsp").forward(req, resp);
-//		resp.sendRedirect("/login.jsp");
+		// req.getRequestDispatcher("/login.jsp").forward(req, resp);
+		// resp.sendRedirect("/login.jsp");
 	}
 }

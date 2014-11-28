8b5a4772353fde8578a672ff1528c40f91ab1a04
kylong <kylong@ctrip.com>
2014-11-28 15:39:26 +0800

    group and noc

--- schedule/src/main/java/com/taobao/zeus/broadcast/alarm/SMSAlarm.java
+++ schedule/src/main/java/com/taobao/zeus/broadcast/alarm/SMSAlarm.java
@@ -1,9 +1,7 @@
 package com.taobao.zeus.broadcast.alarm;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.InetAddress;
@@ -20,6 +18,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.gson.Gson;
 import com.taobao.zeus.store.UserManager;
 import com.taobao.zeus.store.mysql.persistence.ZeusUser;
 import com.taobao.zeus.util.Environment;
@@ -45,43 +44,47 @@ public class SMSAlarm extends AbstractZeusAlarm{
 		List<ZeusUser> userList = userManager.findListByUid(uids);
 		if(userList != null && userList.size()>0){
 			for(ZeusUser user : userList){
-				message += "<br/>负责人："+user.getName()+" 电话："+user.getPhone()+" 邮箱："+user.getEmail();
+				message += "<br>负责人："+user.getName()+" 电话："+user.getPhone()+" 邮箱："+user.getEmail();
 			}
 		}
-		message += "<br/>" + content;
+		message += "<br>" + content;
 		sendNOCAlarm(notifyUrl, accessToken, srcId, devId, itemId, level, message);
 	}
 
 	@SuppressWarnings("deprecation")
 	public void sendNOCAlarm(String sendUrl, String accessToken, String srcId, String devId, String itemId, String level, String message) {
 		log.info("begin to send the noc, the srcId is " + srcId + ", the devId is " + devId + ", the itemId is " + itemId + ".");
-		log.info("The message is " + message);
+		log.info("the message is " + message);
+		log.info("the sendUrl is " + sendUrl);
         HttpClient client = new HttpClient();
 		PostMethod method = new PostMethod(sendUrl);
+		Gson gson = new Gson();
 		method.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		try {
 			Map<String, String> bodyMap = new HashMap<String, String>();
 			bodyMap.put("access_token", accessToken);
-			bodyMap.put("request_body", "njson=" + getRequestBody(srcId, devId, itemId, level, message));
+			String requestBody = getRequestBody(srcId, devId, itemId, level, message);
+			bodyMap.put("request_body", "njson=" + requestBody);
 			method.setRequestBody(JsonUtil.map2json(bodyMap).toString());
 			int code = client.executeMethod(method);
-			log.info("The return code is " + HttpStatus.SC_OK);
-			BufferedReader in = new BufferedReader(new InputStreamReader(method.getResponseBodyAsStream()));
-			String responseBodyAsString = null;
-			while ((responseBodyAsString = in.readLine()) != null) {
-				log.info("The response body is " + responseBodyAsString);
-			}	
-			if (code !=  HttpStatus.SC_OK) {
-				log.error("send cats failed, code: " + code);
+			log.info("the return code is " + HttpStatus.SC_OK);
+			String responseBodyAsString = method.getResponseBodyAsString(2000);
+			log.info("the return is " + responseBodyAsString);
+			ReturnJson rJ = null;
+			if (responseBodyAsString != null) {
+				rJ = gson.fromJson(responseBodyAsString, ReturnJson.class);
+			}
+			if (code !=  HttpStatus.SC_OK || rJ == null || !rJ.isSuccess()) {
+				log.error("send noc failed, code: " + code);
 				return;
 			}
 			log.info("Send noc successfully!");
 		} catch(HttpException  e) {
-			log.error("send noc fail," + e.getMessage());
+			log.error("send noc fail,", e);
 		} catch (IOException e) {
-			log.error("send noc fail," + e.getMessage());
+			log.error("send noc fail,", e);
 		} catch (Exception e) {
-			log.error("send noc fail," + e.getMessage());
+			log.error("send noc fail,", e);
 		}
    }
    
@@ -92,9 +95,62 @@ public class SMSAlarm extends AbstractZeusAlarm{
 		param.put("item_id", itemId);
 		param.put("level",   level);
 		param.put("message", message);
-		param.put("fields",  "[]");
+		param.put("fields",  "");
 //		System.out.println(JsonUtil.map2json(param).toString());
 		return URLEncoder.encode(JsonUtil.map2json(param).toString(), "utf-8");
 	}
+	
+	class ReturnJson{
+
+		private String message;
+		private String data;
+		private boolean success;
+		private int error;
+		
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
+		public String getData() {
+			return data;
+		}
+		public void setData(String data) {
+			this.data = data;
+		}
+		public boolean isSuccess() {
+			return success;
+		}
+		public void setSuccess(boolean success) {
+			this.success = success;
+		}
+		public int getError() {
+			return error;
+		}
+		public void setError(int error) {
+			this.error = error;
+		}
+
+		@Override
+		public String toString() {
+			return "ReturnJson [message=" + message + ", data=" + data
+					+ ", success=" + success + ", error=" + error + "]";
+		}
+	}
+	
+	public static void main(String[] args) {
+		String returnString = "{\"message\": \"\", \"data\": \"enqueue\", \"success\": true, \"error\": 0}";
+//		String returnString = null;
+		Gson gson = new Gson();
+		ReturnJson rJson = gson.fromJson(returnString, ReturnJson.class);
+		System.out.println("the message is " + rJson.getMessage());
+		System.out.println("the data is " + rJson.getData());
+		System.out.println("the error is " + rJson.getError());
+		System.out.println("the success is " + rJson.isSuccess());
+
+	}
+	
+	
 
 }

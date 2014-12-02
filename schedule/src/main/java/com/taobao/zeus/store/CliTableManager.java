e75cf4d930563a78f60605c8d7cbfb82579ac0de
kylong <kylong@ctrip.com>
2014-12-02 16:15:22 +0800

    modify

--- schedule/src/main/java/com/taobao/zeus/store/CliTableManager.java
+++ schedule/src/main/java/com/taobao/zeus/store/CliTableManager.java
@@ -36,11 +36,8 @@ public class CliTableManager implements TableManager {
         log.debug("hive conf file:"+f.toString());
 		if(f.exists()){
 			conf.addResource(f.toURI().toURL());
-			client=new HiveMetaStoreClient(conf);
-		}else{
-			log.info("the file is not exist");
 		}
-		
+		client=new HiveMetaStoreClient(conf);
 	}
 
 	public CliTableManager(Configuration conf) throws Exception {
